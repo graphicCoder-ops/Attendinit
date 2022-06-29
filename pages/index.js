6ba72663917936c8ed2f25ab680deb5489444a3e
graphicCoder-ops <graphic3coder@gmail.com>
2022-06-29 12:40:44 -0400

    button implement

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='w-auto h-screen bg-blue-400 text-black flex justify-center items-center'><div className=' w-full sm:w-1/4 rounded-lg h-2/3 bg-white flex-initial flex items-center flex-col  justify-evenly'>
-<div className='flex-start text-black-400 font-popins text-6xl '>Login</div>
+    <div className='w-auto h-screen font-sans  bg-blue-400 text-black flex justify-center items-center'><div className=' w-full sm:w-1/4 rounded-lg h-2/3 bg-white flex-initial flex items-center flex-col  justify-evenly'>
+<div className='flex-start text-black-400  text-6xl '>Login</div>
  <form className='flex flex-col items-center gap-5' >
    
  <input className='flex-initial input-md border-2 border-zinc-500 rounded-md input w-full max-w-xs'datatheme="light" type="text" placeholder="Enter Your Name" ></input>
  <input className='flex-initial input-md border-2  border-zinc-500 rounded-md input w-full max-w-xs 'type="text" placeholder="Enter Your Password" ></input>
 
-<button className='flex-inital btn-md flex-1 bg-emerald-600 text-white rounded-md'>Click me!</button>
+<button className='my-8 flex-inital btn-md flex-1 bg-gradient-to-r from-emerald-600 to-green-500 focus:bg-gradient-to-l text-white rounded-md'>Click me!</button>
  </form>
     
     </div></div>
