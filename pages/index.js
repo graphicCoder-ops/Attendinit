f106852e8a29d2f3f9ef47a212ab5e059d060a7a
graphicCoder-ops <graphic3coder@gmail.com>
2022-06-29 00:22:29 -0400

    sss

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className='w-auto h-screen bg-blue-400 text-black flex justify-center items-center'><div className=' w-1/4 rounded-lg h-2/3 bg-white flex-initial flex items-center flex-col  justify-evenly'>
-<div className='flex-start text-black-400 font-popins text-6xl '>Login/SignUp</div>
+<div className='flex-start text-black-400 font-popins text-6xl '>Login</div>
  <form className='flex flex-col items-center gap-5' >
    
  <input className='flex-initial input-md border-2 border-black-300 rounded-md input w-full max-w-xs'type="text" placeholder="Enter Your Name" ></input>
--- pages/main.js
+++ pages/main.js
@@ -2,8 +2,9 @@ import Card from "../components/card"
 export default function Home(){
     return(
         
-        <div className=" bg-wall1 bg-contain h-screen w-screen flex justify-center items-center">
+        <div className=" bg-wall1 bg-contain h-screen w-full flex justify-center items-center">
             <div className="fixed top-0 left-0 z-10 text-[#4c3626] backdrop-brightness-100 backdrop-blur-sm bg-white/30 w-screen p-4 font-bold-800  px-10 drop-shadow-md text-7xl">Sessions</div>
+         
           <div className="grid grid-cols-2 gap-10 w-2/3">   <Card number="1"/>
              <Card number="2"/> 
              <Card number="3"/> 
